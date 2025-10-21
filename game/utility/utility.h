cb31a85c4144a4c377df724aa194b4c0d1d84e52
PB&J(*this)-sandwich <140750964+PBJ-Labs@users.noreply.github.com>
2025-10-21 08:40:26 -0500

    Create utility.h

---
+++ game/utility/utility.h
@@ -0,0 +1,26 @@
+//-----------------------------------
+// game : ctkc-1987
+// utility.h, Praise The Lord Not Me
+// author : ???
+//-----------------------------------
+
+namespace game
+{
+  int _gId = 0;
+
+  namespace utility
+  {
+
+    class level {
+    public:
+        level();
+        virtual ~level();
+
+        int _mlvlId = 0;
+    }
+
+  }
+
+
+}
+
